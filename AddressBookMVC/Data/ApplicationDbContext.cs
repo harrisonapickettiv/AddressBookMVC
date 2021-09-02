bd9503de44c908e82388e87775fd3c7d2b7f343c
Harrison A. Pickett, IV <38798125+harrisonapickettiv@users.noreply.github.com>
2021-09-02 15:12:19 -0500

    Add Contact model and associated migration

--- AddressBookMVC/Data/ApplicationDbContext.cs
+++ AddressBookMVC/Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
+using AddressBookMVC.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AddressBookMVC.Data
 {
   public class ApplicationDbContext : DbContext
   {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Contact> Contacts { get; set; }
   }
 }
